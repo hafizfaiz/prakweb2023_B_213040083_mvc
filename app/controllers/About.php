b56bcf39445a6bd16c22231630a8f5b993ea5001
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-23 22:30:09 +0700

    videoPHP#4Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+class About{
+    public function index($nama ='Hafizh',$pekerjaan = 'Mahasiswa'){
+        echo"Hallo, nama saya $nama, saya seorang $pekerjaan";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
+?>
