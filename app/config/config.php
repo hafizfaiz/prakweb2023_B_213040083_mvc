53879973e5954b2a52c9af0189dcafbeb7b6c35a
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-25 23:20:49 +0700

    video2mvc

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/prakweb2023_B_213040083_mvc/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'phpmvc');
--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
-<?php 
-if( !session_id() ) session_start();
-
+<?php
 require_once '../app/init.php';
 
 $app = new App;
