c697c2ec365ceff7663d53e24d1005893bb7fbf6
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-26 15:12:55 +0700

    video6mvc

---
+++ app/core/Constans.php
@@ -0,0 +1,3 @@
+<?php 
+define('BASEURL','http://localhost/prakweb2023_B_213040083_mvc/public/');
+?>
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/Hafizh.png" alt="Hafizh Faiz F" width="200" class="rounded-circle shadow">
     <p>Hallo, nama saya <?= $data['nama'];?>,umur saya <?= $data['umur']; ?> Tahun, saya adalah seorang <?=$data['pekerjaan'];?></p>
+
+</div>
