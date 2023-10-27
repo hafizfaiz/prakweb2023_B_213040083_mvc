412680e1cf053599bb57b9a14573802f3c861075
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-27 23:58:41 +0700

    video8mvc

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASEURL','http://localhost/prakweb2023_B_213040083_mvc/public/');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+?>
--- app/core/Constans.php
+++
@@ -1,3 +0,0 @@
-<?php 
-define('BASEURL','http://localhost/prakweb2023_B_213040083_mvc/public/');
-?>
