89f2d2cffc79369ca5ac8bc00f7c0eb14677af43
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-28 12:25:50 +0700

    video11mvc

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -19,7 +19,8 @@
             <?php foreach( $data['mhs'] as $mhs ) : ?>
               <li class="list-group-item">
                   <?= $mhs['nama']; ?>
-                  <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right">detail</a>
+                  <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm ('yakin ?');">Hapus</a>
+                  <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">detail</a>
               </li>
             <?php endforeach; ?>
           </ul>      
